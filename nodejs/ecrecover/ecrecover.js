582f8e26875cdddb216600e7b74fb3aee45a1fa1
leon-do <leondo123@gmail.com>
2019-07-06 10:39:20 -0400

    lint

--- nodejs/ecrecover/ecrecover.js
+++ nodejs/ecrecover/ecrecover.js
@@ -9,29 +9,29 @@
         }
     }
 */
-const ethers = require('ethers')
+const ethers = require("ethers");
 
-const signedMessage = signMessage('0xdc04977a2078c8ffdf086d618d1f961b6c546222' ,123)
-console.log(signedMessage)
+const signedMessage = signMessage(
+  "0xdc04977a2078c8ffdf086d618d1f961b6c546222",
+  123
+);
+console.log(signedMessage);
 
 function signMessage(_contract, _value) {
-    const privateKey = '0x0123456789012345678901234567890123456789012345678901234567890123'
-    const signingKey = new ethers.SigningKey(privateKey);
-    const address = signingKey.address
+  const privateKey =
+    "0x0123456789012345678901234567890123456789012345678901234567890123";
+  const signingKey = new ethers.SigningKey(privateKey);
+  const address = signingKey.address;
 
-    // hash message
-    const h = ethers.utils.solidityKeccak256(['address', 'int'], [_contract, _value]);
+  // hash message
+  const h = ethers.utils.solidityKeccak256(
+    ["address", "int"],
+    [_contract, _value]
+  );
 
-    // sign and split message: https://github.com/ethers-io/ethers.js/issues/85
-    const {r, s, recoveryParam} = signingKey.signDigest(h);
-    const v = 27 + recoveryParam
+  // sign and split message: https://github.com/ethers-io/ethers.js/issues/85
+  const { r, s, recoveryParam } = signingKey.signDigest(h);
+  const v = 27 + recoveryParam;
 
-    return { h, v, r, s, address }
+  return { h, v, r, s, address };
 }
-
-
-
-
-
-
-
